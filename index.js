6d59236efd6edad078ea455b74e08440005d2c57
SR Shihab <84579722+wb0707@users.noreply.github.com>
2021-08-16 18:50:21 +0600

    sufian

---
+++ index.js
@@ -0,0 +1,17 @@
+
+/*const nav = document.querySelector(".res-nav");
+const hum = document.querySelector(".hamburger");
+*/
+
+ 
+
+function hum() {
+    var show = document.querySelector('.nav-ul');
+
+    if(show.style.display === 'block'){
+        show.style.display = 'none';
+    }
+    else{
+        show.style.display = 'block';
+    }
+};
